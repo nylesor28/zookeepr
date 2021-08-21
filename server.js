c970c72a44b73f25ff0d66ee77f213f764a49929
RD <81473475+nylesor28@users.noreply.github.com>
2021-08-21 19:25:50 -0400

    add public files and fetch logic

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -107,6 +108,21 @@ app.post('/api/animals', (req,res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals',(req, res)=>{
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+app.get('/zookeepers',(req, res)=>{
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, ()=> {
     console.log(`API server now on port ${PORT}!`);
